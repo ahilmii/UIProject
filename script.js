f62c9893211464cf19a111248ae860b14846c26a
ahilmii <ahmet1905hilmi@gmail.com>
2025-05-18 01:16:07 +0300

    navbar duzeltildi ve products-detail sayfası geldi

--- script.js
+++ script.js
@@ -24,3 +24,20 @@ document.querySelectorAll(".sepeteEkle").forEach( function(button) { // bu satı
             });
         }
     )
+
+
+
+
+    // Mobilde Products’a tıklayınca alt menü aç/kapat
+document.addEventListener("DOMContentLoaded", function () {
+  const productMenuItem = document.querySelector("li.product > a");
+
+  productMenuItem.addEventListener("click", function (e) {
+    // Eğer ekran genişliği mobilse
+    if (window.innerWidth <= 780) {
+      e.preventDefault(); // "product.html"e gitmesini engelle
+      const parentLi = this.parentElement;
+      parentLi.classList.toggle("open"); // class ekle/kaldır
+    }
+  });
+});    
